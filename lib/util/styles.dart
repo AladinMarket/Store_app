db37e6e029350bbdd4dcce1bfbdc9c7cd6c8421f
AladinMarket <oasis.burkina@gmail.com>
2024-02-17 10:50:20 +0000

    Vendor App first commit

---
+++ lib/util/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const robotoRegular = TextStyle(
+  fontFamily: 'Ubuntu',
+  fontWeight: FontWeight.w400,
+);
+
+const robotoMedium = TextStyle(
+  fontFamily: 'Ubuntu',
+  fontWeight: FontWeight.w500,
+);
+
+const robotoBold = TextStyle(
+  fontFamily: 'Ubuntu',
+  fontWeight: FontWeight.w700,
+);
+
+const robotoBlack = TextStyle(
+  fontFamily: 'Ubuntu',
+  fontWeight: FontWeight.w900,
+);
